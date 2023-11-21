f1e4186f97eb6e0c5cfb4b59f1aa4a5f79d34c81
ghouddan <ghouddanmohamed90@gmail.com>
2023-11-21 20:06:21 +0000

    add file nesecry for the task

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""s module defines a Square class"""
+
+
+class Square:
+    """This class is an empty Square class"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" defines a Square class"""
+
+
+class Square:
+    """Square class with a private instance attribute"""
+    def __init__(self, size):
+        """Initializes the data"""
+        self.__size = size
